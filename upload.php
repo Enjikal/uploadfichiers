c87e200f3d495dcba70c1c31348df72ebbd96143
enjikal <franck.laurent07@orange.fr>
2023-03-07 14:33:22 +0100

    modificationphp

--- upload.php
+++ upload.php
@@ -5,14 +5,21 @@
         $file_name = $_FILES['fichier']['name'];
         $file_extension = strrchr($file_name, ".");
         
+        $file_tmp_name = $_FILES['fichier']['tmp_name'];
+        $file_dest = 'upload/'.$_file_name;
+        
         $extensions_autorisees = array('.JPG','.jpeg','.png','.gif','.txt','.doc','.pdf');
         echo 'Nom: '.$file_name. '<br/>';
 
         if(in_array($file_extension, $extensions_autorisees)){
-
-        }else{
+            if(move_uploaded_file($file_tmp_name, $file_dest)) {
+                echo 'Fichier envoyé';
+            }else{
             echo 'Format du contenu incorrect';
         }
+    }else{
+        echo 'Les fichiers doivent être au format .JPG, .jpeg, .png, .gif, .txt, .doc, .pdf';
     }
+}  
 ?>
 
