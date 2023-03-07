0c62fe4601e8e84cc2f34c2010d97eea3c9d16aa
enjikal <franck.laurent07@orange.fr>
2023-03-07 17:34:48 +0100

    jysuispresque

--- upload.php
+++ upload.php
@@ -1,25 +1,32 @@
 <?php
-    //var_dump($_FILES);
+    var_dump($_FILES);
 
     if(!empty($_FILES)) {
-        $file_name = $_FILES['fichier']['name'];
+        $file_name = $_FILES['file']['name'];
         $file_extension = strrchr($file_name, ".");
         
-        $file_tmp_name = $_FILES['fichier']['tmp_name'];
-        $file_dest = 'upload/'.$_file_name;
+        var_dump($file_name);
+        $file_tmp_name = $_FILES['file']['tmp_name'];
+        $file_dest = 'upload/'.$file_name;
         
         $extensions_autorisees = array('.JPG','.jpeg','.png','.gif','.txt','.doc','.pdf');
         echo 'Nom: '.$file_name. '<br/>';
 
-        if(in_array($file_extension, $extensions_autorisees)){
+        if(in_array($file_extension, $extensions_autorisees)) {
             if(move_uploaded_file($file_tmp_name, $file_dest)) {
                 echo 'Fichier envoyé';
-            }else{
-            echo 'Format du contenu incorrect';
+            }
+            else {
+                echo 'Format du contenu incorrect';
+            }
         }
-    }else{
+    
+    }
+    else
+    {
         echo 'Les fichiers doivent être au format .JPG, .jpeg, .png, .gif, .txt, .doc, .pdf';
     }
-}  
-?>
+    
 
+
+?>
